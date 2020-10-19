a428d0e7eea5f18ab6ed1832f31e06a07fcf319f
phantuyen <49057691+phantuyen@users.noreply.github.com>
2020-10-19 21:33:38 +0700

    update product

--- online-shopping/src/main/java/com/nhat/backend/onlineshopping/controller/ProductController.java
+++ online-shopping/src/main/java/com/nhat/backend/onlineshopping/controller/ProductController.java
@@ -9,11 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nhat.backend.onlineshopping.exception.MyResourceNotFoundException;
 import com.nhat.backend.onlineshopping.model.Product;
 import com.nhat.backend.onlineshopping.repository.ProductsRepository;
 
@@ -42,4 +45,26 @@ public class ProductController {
 	public Product createProduct(@RequestBody Product product){
 		return productsRepository.save(product);
 	}
+	// get product by id rest api
+	 @GetMapping("/products/{id}")
+	 public ResponseEntity<Product> getProductById(@PathVariable Long id){ 
+	 Product product = productsRepository.findById(id)
+			 .orElseThrow(() -> new MyResourceNotFoundException("Product not exist with id :"+id ));
+	return ResponseEntity.ok(product);		
+}
+		// update product  rest api
+	 @PutMapping("/products/{id}")
+		 public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails){ 
+		 Product product = productsRepository.findById(id)
+				 .orElseThrow(() -> new MyResourceNotFoundException("Product not exist with id :"+id ));
+		 product.setName(productDetails.getName());
+		 product.setPrice(productDetails.getPrice());
+		 product.setSize(productDetails.getSize());
+		 product.setDescription(productDetails.getDescription());
+		 
+		
+		 Product updatedProduct = productsRepository.save(product);
+		return ResponseEntity.ok(updatedProduct);		
+	 }
+	
 }
