05388e3e42fcec56336c121290855f23a90cc4bf
NguyenTranThanhTruc <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2020-10-19 20:18:46 +0700

    update contact on nav

--- online-shopping-web/src/components/layout/Navbar.js
+++ online-shopping-web/src/components/layout/Navbar.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+
+
 
 const Navbar = () => {
+  const show = () => {
+    if (window.alert("Liên hệ với chúng tôi: 0345678912.") == true) {
+      this.alert.onCancel();
+    }
+  }
+
   return (
     <nav className='navbar'>
       <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i>Trang chủ
-        </Link>
+        <Link to='/'>Trang chủ</Link>
       </h1>
       <ul>
         <li>
@@ -16,9 +23,15 @@ const Navbar = () => {
         <li>
           <Link to='/dangnhap'>Đăng nhập</Link>
         </li>
+        <li>
+          <Link onClick={()=>show()}>Liên hệ</Link>
+        </li>
       </ul>
+
+
     </nav>
   );
 };
 
 export default Navbar;
+
