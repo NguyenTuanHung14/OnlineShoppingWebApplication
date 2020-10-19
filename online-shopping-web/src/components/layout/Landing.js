669f3dab6170e85576775107529ccc1c92ee996c
NguyenTranThanhTruc <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2020-10-19 14:20:07 +0700

    header

---
+++ online-shopping-web/src/components/layout/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">LIST PRODUCTS</h1>
+          <p className="lead">
+            PRODUCTS
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+ 
+export default Landing;
